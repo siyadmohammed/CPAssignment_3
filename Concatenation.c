84c8775a985b40f6717d85f9b31baad9b1d4c742
MOHAMMED SIYAD <62105822+siyadmohammed@users.noreply.github.com>
2020-06-06 19:11:49 +0530

    Update Concatenation.c

--- Concatenation.c
+++ Concatenation.c
@@ -1,32 +1,22 @@
 #include<stdio.h>
-
-void main()
-	{
-		char str1[100], str2[25];
-		int i,j;
-
-		printf("\n Enter First String: ");
-		gets(str1);
-		printf("\n Enter Second String: ");
-		gets(str2);
-
-		while(str1[i]!='\0')
-  			{
-   				 i++;
-  			}
- 		
-
-  		
-     while(str2[j]!='\0')
-  			{
-   				 str1[i]=str2[j];
-    				 j++;
-    				 i++;
-  			}
-		str1[i] = '\0'; 
-    
-    
-    
-		printf("\n The concatenated string is: %s \n ", str1);
-		
-	}
+int main()
+{
+    int i=0,j=0;
+    char str1[50],str2[50];
+    printf("Enter 2 strings ending with $\n");
+    gets(str1);
+    gets(str2);
+    while(str1[i]!='\0')
+    {
+      i++;
+    }
+    while(str2[j]!='\0')
+    {
+     str1[i]=str2[j];
+     i++;
+     j++;
+    }
+    str1[i]='\0';
+    puts(str1);
+    return 0;
+}
