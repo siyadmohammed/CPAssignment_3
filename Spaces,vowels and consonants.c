830a49543d6423453f1a28974f420857395fcd31
MOHAMMED SIYAD <62105822+siyadmohammed@users.noreply.github.com>
2020-06-06 19:13:51 +0530

    Create Spaces,vowels and consonants.c

---
+++ Spaces,vowels and consonants.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+  {
+    int vowel=0,consonant=0,space=0,i;
+    char str[100];
+    printf("\n Enter a string ");
+    gets(str);
+    for(i=0;str[i]!='\0';i++)
+      {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+          {
+            vowel++;
+          }
+        else if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+          {
+            consonant++;
+          }
+        else
+          {
+            space++;
+          }
+      }
+    printf("\n There are %d vowels",vowel);
+    printf("\n There are %d consonants",consonant);
+    printf("\n There are %d spaces",space);
+ }
